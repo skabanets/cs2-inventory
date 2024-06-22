135a01f586c91bd4da155cc28aea5ec10309bb56
Sergey Kabanets <skabanets97@gmail.com>
2024-06-22 05:11:23 +0300

    add styles for weapon info in modal-window

--- src/js/weaponContentTemplate.js
+++ src/js/weaponContentTemplate.js
@@ -2,12 +2,17 @@ const weaponContentTemplate = weapon => {
   const { name, exterior, price, description, collection, image } = weapon;
 
   return `
-        <img src="${image}" alt="${name}"/>
-        <h2>${name}</h2>
-        <p>${exterior}</p>
-        <p>${price}</p>
-        <p>${description}</p>
-        <p>${collection}</p>`;
+    <img src="${image}" alt="${name}" class='weapon-modal-image'/>
+    <div class="weapon-info">
+      <h2>${name}</h2>
+      <div class='weapon-details'>
+        <p>Exterior: ${exterior}</p>
+        <p>Price: ${price}$</p>
+      </div>
+      <p>${description}</p>
+    </div>
+    <a href="https://example.com" target="_blank" class="market-link">Inspect in market</a>
+    `;
 };
 
 export default weaponContentTemplate;
